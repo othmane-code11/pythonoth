63789ee02cfb0d6bbaaf9f1a9b2c8e1eec7a1d58
ezzaytouni othmane <ezzaytouniothmane@gmail.com>
2025-01-23 18:41:10 +0100

    just learn

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-const tarifs = {
-    'Safi': 90,
-    'Marrakech': 120,
-    'Mohammédia': 20,
-    'Rabat': 60
-};
-console.log(tarifs[Rabat]);
+let produits = [
+    { nom: "Pomme", quantite: 5 },
+    { nom: "Banane", quantite: 0 },
+    { nom: "Orange", quantite: 10 },
+];
+function trouver(lst, name) {
+    let ex = false;
+    for (let obj in lst) {
+        if (lst[obj].quantite != 0 && lst[obj].nom == name) {
+            ex = true;
+            break;
+        }
+    }
+    if (ex == true) {
+            console.log(`${name} exist`);
+    }
+    else {
+            console.log(`${name} non exist`);
+    }
+}
+
+trouver(produits, 'Banane')
